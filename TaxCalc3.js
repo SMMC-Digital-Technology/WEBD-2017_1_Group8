e1b89b2ec4b08eac5df60d060e5afe100d63a16a
British <HUMEBP@mackillop.act.edu.au>
2017-06-01 10:02:54 +1000

    Add files via upload

--- TaxCalc3.js
+++ TaxCalc3.js
@@ -1,10 +1,6 @@
-var inputPay = document.getElementById("inputPay");
-var outputTax = document.getElementById("outputTax");
+var nameInput = document.getElementById("inputPay");
+var outputLine = document.getElementById("outputTax");
 
 function buttonPress() {
-    var number = numberInput.value;
-    if (number < 10) {
-        number = number * 10;
-    }
-    outputLine.value = number;
+    
 }
