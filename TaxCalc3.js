739b4ef219cf5b2006fd181862d7017dbd474b08
zyster <MARSICZA@mackillop.act.edu.au>
2017-06-02 14:28:43 +1000

    Add files via upload
    version 0.5 - added support for bracket 1 and bracket 2

--- TaxCalc3.js
+++ TaxCalc3.js
@@ -1,6 +1,15 @@
-var nameInput = document.getElementById("inputPay");
-var outputLine = document.getElementById("outputTax");
+var inputPay = document.getElementById("inputPay");
+var outputTax = document.getElementById("outputTax");
 
 function buttonPress() {
-    
+
+    if (inputPay.value < 16000) {
+        outputTax.value = 0;
+    } else {
+        if (inputPay.value < 52500) {
+            var bracket2 = inputPay.value - 16000
+            var outputBracket2 = 0.10 * bracket2;
+            outputTax.value = outputBracket2; 
+        }
+    }
 }
