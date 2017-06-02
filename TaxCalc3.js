d37da5ba2fe6ff7f51e8371e53d4b58478741751
zyster <MARSICZA@mackillop.act.edu.au>
2017-06-02 21:27:21 +1000

    Add files via upload
    made the output to two decimal places only.

--- TaxCalc3.js
+++ TaxCalc3.js
@@ -1,15 +1,16 @@
+
 var inputPay = document.getElementById("inputPay");
 var outputTax = document.getElementById("outputTax");
 
 function buttonPress() {
 
     if (inputPay.value < 16000) {
+
         outputTax.value = 0;
     } else {
         if (inputPay.value < 52500) {
-            var bracket2 = inputPay.value - 16000
-            var outputBracket2 = 0.10 * bracket2;
-            outputTax.value = outputBracket2; 
+            var bracket2 = (inputPay.value - 16000) * 0.1;
+            outputTax.value = bracket2.toFixed(2);
         }
     }
 }
