5030b8805615c8587ac9008c557f90c0f41d8598
zyster <MARSICZA@mackillop.act.edu.au>
2017-06-02 21:45:10 +1000

    Add files via upload
    completed all of the algorithm.

--- TaxCalc3.js
+++ TaxCalc3.js
@@ -11,6 +11,19 @@ function buttonPress() {
         if (inputPay.value < 52500) {
             var bracket2 = (inputPay.value - 16000) * 0.1;
             outputTax.value = bracket2.toFixed(2);
+        } else {
+            if (inputPay.value < 113000) {
+                var bracket3 = (inputPay.value - 52500) * 0.16 + 3650;
+                outputTax.value = bracket3.toFixed(2);
+            } else {
+                if (inputPay.value < 184500) {
+                    var bracket4 = (inputPay.value - 113000) * 0.24 + 13330;
+                    outputTax.value = bracket4.toFixed(2);
+                } else {
+                    var bracket5 = (inputPay.value - 184500) * 0.36 + 30490;
+                    outputTax.value = bracket5.toFixed(2);
+                }
+            }
         }
     }
 }
