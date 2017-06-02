3b814727807bcd8043598d19895ca2c60490cf64
zyster <MARSICZA@mackillop.act.edu.au>
2017-06-02 21:53:48 +1000

    Add files via upload
    added comments to the javascript file

--- TaxCalc3.js
+++ TaxCalc3.js
@@ -1,25 +1,27 @@
-
+//defines the two variables we use inside of this js file from html
 var inputPay = document.getElementById("inputPay");
 var outputTax = document.getElementById("outputTax");
 
+
+// starts the function when the button is pressed, all of the tax working on goes on inside of the if statements
 function buttonPress() {
 
     if (inputPay.value < 16000) {
 
         outputTax.value = 0;
     } else {
-        if (inputPay.value < 52500) {
+        if (inputPay.value < 52500) { //works out the secound bracket
             var bracket2 = (inputPay.value - 16000) * 0.1;
             outputTax.value = bracket2.toFixed(2);
         } else {
-            if (inputPay.value < 113000) {
+            if (inputPay.value < 113000) { //works out the third bracket
                 var bracket3 = (inputPay.value - 52500) * 0.16 + 3650;
                 outputTax.value = bracket3.toFixed(2);
             } else {
-                if (inputPay.value < 184500) {
+                if (inputPay.value < 184500) { //works out the fourth bracket
                     var bracket4 = (inputPay.value - 113000) * 0.24 + 13330;
                     outputTax.value = bracket4.toFixed(2);
-                } else {
+                } else { //works out the fith and last bracket
                     var bracket5 = (inputPay.value - 184500) * 0.36 + 30490;
                     outputTax.value = bracket5.toFixed(2);
                 }
